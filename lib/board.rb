945d6f645f73bb2494c87a1afd139ee9afe44a8a
Dylan <152871121+bluemayo@users.noreply.github.com>
2024-05-01 23:50:08 +0800

    Added method to update player variables

--- lib/board.rb
+++ lib/board.rb
@@ -1,16 +1,35 @@
 # frozen_string_literal: true
 
 require_relative 'display'
+require_relative 'player'
 
 # Defines the board of connect four
 class Board
-  include display
+  include Display
 
-  def initialize(player1, player2)
+  def initialize(player1 = nil, player2 = nil)
     @player1 = player1
     @player2 = player2
     @board = Array.new(7).map { Array.new(6) }
   end
 
-  def play; end
+  def play
+    update_players
+  end
+
+  def update_players
+    (1..2).each do |player_number|
+      new_player = create_player(input_name(player_number))
+      player_number == 1 ? @player1 = new_player : @player2 = new_player
+    end
+  end
+
+  def create_player(name)
+    Player.new(name)
+  end
+
+  def input_name(player_number)
+    print "Enter Player#{player_number} Name: "
+    gets.chomp
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,7 +3,10 @@
 require_relative '../lib/board'
 
 # This file tests the methods in the Board class
-describe Board do
+describe Board do # rubocop: disable Metrics/BlockLength
+  # generic Board object
+  subject(:game) { described_class.new }
+
   describe '#initialize' do
     # Only creates instance variables.
   end
@@ -12,5 +15,57 @@ describe Board do
     # Public Script Method, methods inside should be tested.
   end
 
+  describe '#update_players' do
+    # located inside #play
+    # Looping Script Method, behavior should be tested.
+    # Command Method, change in state should be tested.
+    before do
+      allow(game).to receive(:input_name)
+    end
 
+    context 'Loops twice' do
+      it 'Calls #input_name twice' do
+        expect(game).to receive(:input_name).twice
+        game.update_players
+      end
+
+      it 'Calls #create_player twice' do
+        expect(game).to receive(:create_player).twice
+        game.update_players
+      end
+    end
+
+    context 'Player variables updated' do
+      # create method stub for #create_player to return fake values
+      before do
+        allow(game).to receive(:create_player).and_return('mayo', 'tddman')
+      end
+
+      it 'Updates @player1' do
+        expect { game.update_players }.to change { game.instance_variable_get(:@player1) }.to('mayo')
+      end
+
+      it 'Updates @player2' do
+        expect { game.update_players }.to change { game.instance_variable_get(:@player2) }.to('tddman')
+      end
+    end
+  end
+
+  describe '#create_player' do
+    # located inside #update_players
+    # Method with outgoing command -> test that a message has been sent.
+    context 'When creating players:' do
+      let(:test_player) { 'mayo' }
+
+      it 'Calls #new on Player class' do
+        expect(Player).to receive(:new).once
+        game.create_player(test_player)
+      end
+    end
+  end
+
+  describe '#input_name' do
+    # located inside #update_players
+    # Only contain puts and gets
+  end
 end
